cbc8b7ec31ddfa217429a9572504388d2930127c
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2025-02-16 14:29:24 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,32 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD7RA-8Swj__dO3Qf3tUrb8zeG_huoQyg8",
+  authDomain: "quick-polling-app.firebaseapp.com",
+  projectId: "quick-polling-app",
+  storageBucket: "quick-polling-app.firebasestorage.app",
+  messagingSenderId: "94879272963",
+  appId: "1:94879272963:web:b47c56307578d9b6093e36",
+  measurementId: "G-JFYDNK5ND6"
 };
-console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+
+
